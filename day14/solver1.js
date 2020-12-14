fc079e1c45df6a4c09f8d4f0daef44dc593033c4
Anton Moberg <anton.moberg@williamhill.com>
2020-12-14 15:54:32 +0100

    day14

--- day14/solver1.js
+++ day14/solver1.js
@@ -1,57 +1,23 @@
 const fs = require('fs')
-const { up } = require('inquirer/lib/utils/readline')
-const { start } = require('repl')
 
-let valArr = []
-fs.readFile('./day11/input.txt', 'UTF-8', function (err, data) {
-    if (err) return console.log(err)
-    valArr = data.split(/\r\n/)
-    while (JSON.stringify(valArr) != JSON.stringify(seatChanger(valArr))){
-        valArr = seatChanger(valArr).slice(); 
-    }
-    console.log(JSON.stringify(valArr).split('#').length-1)
-})
+let data = fs.readFileSync('./input.txt', 'UTF-8').split(/\r\n/)
+let mem = {}
+let bitmask = ''
 
-
-function seatChanger(grid) {
-    let updatedGrid = grid.slice()
-    for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid[y].length; x++) {
-            let adjacents = []
-            let empty = 0
-            let occu = 0
-            adjacents.push([x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
-                [x-1, y], [x+1, y],
-                [x - 1, y + 1], [x, y + 1], [x + 1, y + 1])
-            adjacents.forEach(adj => {
-                if (typeof grid[adj[1]] != 'undefined' && typeof grid[adj[1]][adj[0]] != 'undefined') {
-                    switch (grid[adj[1]][adj[0]]) {
-                        case ('L'):
-                            empty++
-                            break;
-                        case ('#'):
-                            occu++
-                            break;
-                        case ('.'):
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            })
-            switch (grid[y][x]) {
-                case ('L'):
-                    if (occu == 0) updatedGrid[y] = updatedGrid[y].substring(0, x) + '#' + updatedGrid[y].substring(x+1);
-                    break;
-                case ('#'):
-                    if (occu > 3) updatedGrid[y] = updatedGrid[y].substring(0, x) + 'L' + updatedGrid[y].substring(x+1);
-                    break;  
-                case ('.'):
-                    break;
-                default:
-                    break;
+data.forEach(row => {
+    if (row[1] == 'a') {
+        bitmask = row.split(' ')[2]
+    } else if (row[1] == 'e') {
+        var curVal = parseInt(row.split(' ')[2]).toString(2)
+        curVal = new Array(36 - curVal.length + 1).join('0').concat(curVal)
+        for (var i = 0; i < bitmask.length; i++) {
+            if (bitmask[i] != 'X') {
+                curVal = curVal.substr(0, i) + bitmask[i] + curVal.substr(i+1)
             }
         }
+        mem[row.slice(4).split(/]/)[0]] = curVal
     }
-    return updatedGrid;
-}
+})
+var answer = 0
+Object.values(mem).forEach(val => answer += parseInt(val,2))
+console.log(answer)
--- day14/solver2.js
+++ day14/solver2.js
@@ -1,137 +1,33 @@
 const fs = require('fs')
-const { up } = require('inquirer/lib/utils/readline')
-const { start } = require('repl')
 
-let valArr = []
-fs.readFile('./day11/input.txt', 'UTF-8', function (err, data) {
-    if (err) return console.log(err)
-    valArr = data.split(/\r\n/)
-    while (JSON.stringify(valArr) != JSON.stringify(seatChanger(valArr))){
-        valArr = seatChanger(valArr).slice(); 
-    }
-    console.log(JSON.stringify(valArr).split('#').length-1)
-})
+let data = fs.readFileSync('./input.txt', 'UTF-8').split(/\r\n/)
+let mem = {}
+let bitmask = ''
 
-
-function seatChanger(grid) {
-    let updatedGrid = grid.slice()
-    for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid[y].length; x++) {
-            let adjacents = getAdjecants(grid, [y,x])
-            let empty = adjacents[0]
-            let occu = adjacents[1]
-            switch (grid[y][x]) {
-                case ('L'):
-                    if (occu == 0) updatedGrid[y] = updatedGrid[y].substring(0, x) + '#' + updatedGrid[y].substring(x+1);
-                    break;
-                case ('#'):
-                    if (occu > 4) updatedGrid[y] = updatedGrid[y].substring(0, x) + 'L' + updatedGrid[y].substring(x+1);
-                    break;  
-                case ('.'):
-                    break;
-                default:
-                    break;
+data.forEach(row => {
+    if (row[1] == 'a') {
+        bitmask = row.split(' ')[2]
+    } else if (row[1] == 'e') {
+        let initMem = parseInt(row.slice(4).split(/]/)[0]).toString(2)
+        let memAddreses = [new Array(36 - initMem.length + 1).join('0').concat(initMem)]
+        for (var i = 0; i < bitmask.length; i++) {
+            if (bitmask[i] == 'X') {
+                temp = []
+                memAddreses.forEach(addr => {
+                    temp.push(addr.substr(0, i) + '0' + addr.substr(i+1), addr.substr(0, i) + '1' + addr.substr(i+1))
+                });
+                memAddreses = temp.slice()
+            } else if (bitmask[i] == '1') {
+                memAddreses.forEach((addr,j) => {
+                   memAddreses[j] = addr.substr(0, i) + '1' + addr.substr(i+1)
+                });
             }
         }
+        memAddreses.forEach(addr => {
+            mem[parseInt(addr,2)] = parseInt(row.split(' ')[2])
+        })
     }
-    return updatedGrid;
-}
-
-function getAdjecants(grid, cur) {
-    let empty = 0
-    let occu = 0
-
-    var x = cur[1]-1
-    for (let y = cur[0]-1; y >= 0 && x >= 0; y--) {
-        if (grid[y][x] == 'L') {
-            empty++
-            break;
-        } else if (grid[y][x] == '#') {
-            occu++
-            break;
-        }
-        x--
-    }
-
-    direction:
-    for (let y = cur[0]-1; y >= 0; y--) {
-        if (grid[y][cur[1]] == 'L') {
-            empty++
-            break direction;
-        } else if (grid[y][cur[1]] == '#') {
-            occu++
-            break direction;
-        }
-    }
-
-    x = cur[1]+1
-    for (let y = cur[0]-1; y >= 0 && x < grid[0].length; y--) {
-        if (y < 0) break
-        if (grid[y][x] == 'L') {
-            empty++
-            break;
-        } else if (grid[y][x] == '#') {
-            occu++
-            break;
-        }
-        x++
-    }
-
-    direction:
-    for (let x = cur[1]-1; x >= 0; x--) {
-        if (grid[cur[0]][x] == 'L') {
-            empty++
-            break direction;
-        } else if (grid[cur[0]][x] == '#') {
-            occu++
-            break direction;
-        }
-    }
-
-    direction:
-    for (let x = cur[1]+1; x < grid[0].length; x++) {
-        if (grid[cur[0]][x] == 'L') {
-            empty++
-            break direction;
-        } else if (grid[cur[0]][x] == '#') {
-            occu++
-            break direction;
-        }
-    }
-
-    x = cur[1]-1
-    for (let y = cur[0]+1; y < grid.length && x >= 0; y++) {
-        if (grid[y][x] == 'L') {
-            empty++
-            break;
-        } else if (grid[y][x] == '#') {
-            occu++
-            break;
-        }
-        x--
-    }
-
-    direction:
-    for (let y = cur[0]+1; y < grid.length; y++) {
-        if (grid[y][cur[1]] == 'L') {
-            empty++
-            break direction;
-        } else if (grid[y][cur[1]] == '#') {
-            occu++
-            break direction;
-        }
-    }
-
-    x = cur[1]+1
-    for (let y = cur[0]+1; y < grid.length && x < grid[0].length; y++) {
-        if (grid[y][x] == 'L') {
-            empty++
-            break;
-        } else if (grid[y][x] == '#') {
-            occu++
-            break;
-        }
-        x++
-    }
-    return [empty, occu]
-}
+})
+var answer = 0
+Object.values(mem).forEach(val => answer += val)
+console.log(answer)
