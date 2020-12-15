d1f7bfa3ca9606822fb13046978ec67158179966
Anton Moberg <anton.moberg@williamhill.com>
2020-12-15 14:35:46 +0100

    day15 cleanup

--- day15/solver2.js
+++ day15/solver2.js
@@ -1,18 +1,30 @@
 const fs = require('fs')
 
-let data = fs.readFileSync('./input.txt', 'UTF-8').split(/,/).map(Number)
+let dataDict = {}
+let iteration = 0
+let last = 0
+let data = fs.readFileSync('./input.txt', 'UTF-8').split(/,/)
+data.forEach((element, i) => {
+    if (i === data.length-1) {
+        last = element
+        iteration = i
+    } else {
+        dataDict[element] = i
+    }
+});
 
 let lastNumSpoken = 0
 console.time("time")
-while (data.length < 30000000) {
-    var temp = data[data.length - 1]
-    data.pop()
-    index = (data.lastIndexOf(temp) + 1)
-    data.push(temp)
-    lastNumSpoken = index === 0 ? 0 : data.length - index 
-    data.push(lastNumSpoken)
-    if (data.length % 100000 == 0) console.log(data.length, lastNumSpoken)
+while (iteration < 30000000-1) {
+    temp = last
+    if (temp in dataDict) {
+        last = iteration - dataDict[temp] 
+    } else {
+        last = 0
+    }
+    dataDict[temp] = iteration
+    iteration++
+    if (iteration % 1000000 == 0) console.log("iteration: " + iteration)
 }
-console.log(lastNumSpoken)
+console.log(last)
 console.timeEnd("time")
-    
